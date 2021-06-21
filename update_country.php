d551bf1d7c88efd011a5169a43a9e7bc371c42f1
aetham <henri206@hotmail.com>
2021-06-22 00:39:24 +0300

    Added method to update country table

---
+++ update_country.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+require_once("includes/functions.php");
+logged_in();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Mini CMS</title>
+    <link rel="stylesheet" href="assets/style.css"/>
+</head>
+<body>
+<?php
+$mysqli = new mysqli("localhost", "root", "kodukodu12", "cms") or die(mysqli_error($mysqli));
+$id = $_GET['id'];
+$result = mysqli_query($mysqli, "SELECT * FROM country WHERE id=$id");
+
+while ($row = $result->fetch_assoc()) {
+    $name = $row["name"];
+    $continent = $row["continent"];
+    $capital = $row["capital"];
+    $gdp = $row["gdp"];
+}
+?>
+<div class="container">
+    <form action="" method="POST">
+        <input type="hidden" name="id" value="<?php echo $id; ?>">
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name"
+               value="<?php echo $name; ?>"><br>
+
+        <label for="continent">Continent:</label><br>
+        <input type="text" id="continent" name="continent"
+               value="<?php echo $continent; ?>"><br>
+
+        <label for="capital">Capital:</label><br>
+        <input type="text" id="capital" name="capital"
+               value="<?php echo $capital ?>"><br>
+
+        <label for="gdp">GDP:</label><br>
+        <input type="number" id="gdp" name="gdp"
+               value="<?php echo $gdp; ?>"><br/>
+
+        <input type="submit" name="update" value="update">
+    </form>
+    <?php
+    require_once("includes/functions.php");
+    update_country_table();
+    ?>
+
+</div>
+</body>
+</html>
